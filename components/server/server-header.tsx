28c8ec23708635bf99b11eaabb835d344b1dd4ba
ch1spolo <polidavis28@gmail.com>
2024-04-27 23:34:59 -0500

    add leave server modal with trigger

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -101,7 +101,12 @@ const ServerHeader = ({ server, role }: ServerHeaderProps) => {
           </DropdownMenuItem>
         )}
         {!isAdmin && (
-          <DropdownMenuItem className="text-rose-500 px-3 py-2 text-sm cursor-pointer transition-all border-b border-b-transparent hover:border-b-white ">
+          <DropdownMenuItem
+            className="text-rose-500 px-3 py-2 text-sm cursor-pointer transition-all border-b border-b-transparent hover:border-b-white "
+            onClick={() => {
+              onOpen("leaveServer", { server });
+            }}
+          >
             Leave Server
             <LogOut className="h-4 w-4 ml-auto" />
           </DropdownMenuItem>
