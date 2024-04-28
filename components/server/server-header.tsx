8395bebbaafee783f674deaafd1636def7525115
ch1spolo <polidavis28@gmail.com>
2024-04-28 01:02:03 -0500

    delete server modal, and implements delete server

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -95,7 +95,12 @@ const ServerHeader = ({ server, role }: ServerHeaderProps) => {
           </>
         )}
         {isAdmin && (
-          <DropdownMenuItem className="text-rose-500 px-3 py-2 text-sm cursor-pointer transition-all border-b border-b-transparent hover:border-b-white ">
+          <DropdownMenuItem
+            className="text-rose-500 px-3 py-2 text-sm cursor-pointer transition-all border-b border-b-transparent hover:border-b-white "
+            onClick={() => {
+              onOpen("deleteServer", { server });
+            }}
+          >
             Delete Server
             <Trash className="h-4 w-4 ml-auto" />
           </DropdownMenuItem>
